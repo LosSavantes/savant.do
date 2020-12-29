e8b493485be39cb49297de2da8261a68d69caae8
Alex Rain <alex@savant.do>
2020-12-29 19:51:56 -0400

    added fb events

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
@@ -14,15 +14,14 @@ class MyDocument extends Document {
             href="https://unpkg.com/aos@next/dist/aos.css"
             rel="stylesheet"
           />
-          {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+          {/* <!-- Google Analytics --> */}
           <script
             async
             src="https://www.googletagmanager.com/gtag/js?id=G-P7Q5MDFS77"
           ></script>
           <script
             dangerouslySetInnerHTML={{
-              __html: 
-`
+              __html: `
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
@@ -31,6 +30,38 @@ gtag('config', 'G-P7Q5MDFS77');
 `,
             }}
           />
+          {/* <!-- End Google Analytics --> */}
+
+          {/* <!-- Facebook Pixel Code --> */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '384335429300835');
+fbq('track', 'PageView');
+`,
+            }}
+          />
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `
+            <img
+              height="1"
+              width="1"
+              style="display:none"
+              src="https://www.facebook.com/tr?id=384335429300835&ev=PageView&noscript=1"
+            />
+          `,
+            }}
+          />
+          {/* <!-- End Facebook Pixel Code --> */}
         </Head>
         <body>
           <Main />
